8bb566cb52f0eb2193946b62199fef7c4afda74f
ssehuun <shn413@gmail.com>
2020-08-18 10:42:50 +0900

    Update 11399.cpp
    ATM

--- baekjoon/그리디/11399.cpp
+++ baekjoon/그리디/11399.cpp
@@ -1,3 +1,8 @@
+/*
+https://www.acmicpc.net/problem/11399
+접근법 - 시간의 합의 최소값을 구하기 위해서는 돈을 인출하는 시간이 적은 사람부터 앞에 와야 그 다음 사람의 인출시간이 적어진다.
+sort 함수로 정렬 후 앞 사람의 인출 시간의 합과 본인 인출 시간을 더해가면 최종 인출시간이 나온다.
+*/
 #include <cstdio>
 #include <vector>
 #include <iostream>
