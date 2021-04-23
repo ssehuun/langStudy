94fa749a65aacd0174729db75d9ec90da0e68f2e
ssehuun <shn413@gmail.com>
2021-04-23 15:45:39 +0900

    전화번호 목록

--- programmers/hash/42577.py
+++ programmers/hash/42577.py
@@ -8,9 +8,21 @@ def solution(phone_book):
     phone_book.sort(key=len)
     for i in range(len(phone_book)):
         j = phone_book[i]
-        print("j: "+j)
         for k in phone_book[i+1:]:
-            print("k: "+k)
             if j in k: 
                 return False
     return True
+
+
+/* 전화번호가 접두사에 있는것을 확인해야 하므로, 슬라이싱 필요
+ * 정확성: 83.3
+ * 효율성: 16.7
+ * 합계: 100.0 / 100.0
+ */
+def solution(phone_book):
+    answer = True
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        if phone_book[i] in phone_book[i+1][:len(phone_book[i])]:
+            answer = False
+    return answer
