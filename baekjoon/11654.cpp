b57680ebf0b54bc59b7f49227772f702fce75881
ssehuun <shn413@naver.com>
2021-12-27 01:36:01 +0900

    아스키코드(구현)

---
+++ baekjoon/11654.cpp
@@ -0,0 +1,17 @@
+// https://www.acmicpc.net/problem/11654
+// 아스키 코드
+// C방식, C++방식
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main(){
+	char a;
+	// cin >> a;
+	// cout << a-0;
+
+	// scanf("%c", &a);
+	// printf("%d", a);
+	return 0;
+}
